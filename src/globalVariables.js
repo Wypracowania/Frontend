9a51ea892cf5e266ff57437958f14f18c65d655f
Kamil Albrycht <kamilkowicz@gmail.com>
2020-10-22 22:14:39 +0200

    new_url

--- src/globalVariables.js
+++ src/globalVariables.js
@@ -1,8 +1,15 @@
-export const LOGIN_URL = 'http://localhost:8000/login';
-export const ADD_ORDER_URL =
-  'http://localhost:8000/user/nowe-zamowienie';
-export const FETCH_ORDERS_URL = 'http://localhost:8000/user/all';
-export const ORDER_DETAIL_URL = 'http://localhost:8000/user/order/'
+export const LOGIN_URL = 'https://wypracowania.herokuapp.com/login';
+export const REGISTER_URL = 'https://wypracowania.herokuapp.com/rejestracja';
+export const ADD_ORDER_URL = 'http://localhost:8000/api/user/nowe-zamowienie';
+
+// WSZYSTKIE ZAMOWIENIA
+export const FETCH_ORDERS_URL = 'https://wypracowania.herokuapp.com/api/user/all';
+
+// https://wypracowania.herokuapp.com/api/user/order/<id> NIE ZAPOMNIEC O ID ZAMOWIENIA
+export const ORDER_DETAIL_URL = 'https://wypracowania.herokuapp.com/api/user/order/';
+
+// https://wypracowania.herokuapp.com/writer/api/profile_photo/pisarz1 OSTATNI ARGUMENT TO USERNAME
+export const GET_WRITER_PHOTO = 'https://wypracowania.herokuapp.com/writer/api/profile_photo/'
 
 // http://localhost:8000/
 // https://wypracowania.herokuapp.com/
